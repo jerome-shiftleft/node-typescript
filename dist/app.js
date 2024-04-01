168e62c43eddd7a77e7ff893c29baefc16c4219b
jerome-shiftleft <jerome@theshiftleft.com>
2024-04-02 04:12:03 +0800

    progress

--- dist/app.js
+++ dist/app.js
@@ -3,8 +3,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-//const express = require("express");
 const express_1 = __importDefault(require("express"));
+const todos_1 = __importDefault(require("./routes/todos"));
 const app = (0, express_1.default)();
-console.log('node express typescript!');
+console.log("node express typescript!");
+app.use("/todos/", todos_1.default);
+app.use((err, req, res, next) => {
+    res.status(500).json({ message: err.message });
+});
 app.listen(3000);
--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,10 @@
-//const express = require("express");
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
+import todoRoutes from "./routes/todos";
+
 const app = express();
-console.log('node express typescript!');
+console.log("node express typescript!");
+app.use("/todos/", todoRoutes);
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  res.status(500).json({ message: err.message });
+});
 app.listen(3000);
