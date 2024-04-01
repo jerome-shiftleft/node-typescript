7c3c7c2ebf5346a401edb8d473196ee6d435c795
jerome-shiftleft <jerome@theshiftleft.com>
2024-04-02 03:59:37 +0800

    node express typescript initial setup

--- dist/app.js
+++ dist/app.js
@@ -1,2 +1,10 @@
 "use strict";
-console.log("hello world");
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+//const express = require("express");
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+console.log('node express typescript!');
+app.listen(3000);
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,5 @@
-console.log("hello world");
+//const express = require("express");
+import express from "express";
+const app = express();
+console.log('node express typescript!');
+app.listen(3000);
