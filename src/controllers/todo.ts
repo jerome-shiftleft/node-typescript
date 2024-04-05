a6eed7575f0462ad51cd7dff61df7b4a118408b5
jerome-shiftleft <jerome@theshiftleft.com>
2024-04-06 03:19:23 +0800

    cleanup

--- src/controllers/todo.ts
+++ src/controllers/todo.ts
@@ -6,9 +6,9 @@ const TODOS: Todo[] = [];
 export const createTodo: RequestHandler = (req, res, next) => {
   const text = (req.body as { text: string }).text;
   const newTodo = new Todo(Math.random().toString(), text);
-  
+
   TODOS.push(newTodo);
-  
+
   res.status(201).json({
     message: "Created the todo.",
     createdTodo: newTodo,
